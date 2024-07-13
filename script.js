b3feab0c9ef42c1737130ff2caaa5677ce14ab13
valonsocg <valonsocg@gmail.com>
2024-07-13 12:42:56 -0500

    Add logic to play a round and saving scores

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const forms = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
@@ -10,4 +12,41 @@ function getHumanChoice() {
   return answer;
 }
 
-console.log(getHumanChoice());
+/* rock > scissors> paper> rock */
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock smashes Scissors");
+    humanScore++;
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock ");
+    computerScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You Win! Paper beats Rock ");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors cuts Paper ");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock smashes Scissors ");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You Win! Scissors cuts Paper ");
+    humanScore++;
+  } else {
+    console.log("It's a Draw!");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(humanSelection);
+console.log(computerSelection);
+
+playRound(humanSelection, computerSelection);
+console.log(
+  `Your score is:  ${humanScore} and Computer score is: ${computerScore}`
+);
+
+// console.log(playRound());
